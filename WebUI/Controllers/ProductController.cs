cb03fdf77596995d6fadd4261e85bf73596a96cf
LadyHail <ladyhail@outlook.com>
2018-01-05 13:47:21 +0100

    Add pagination

--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -1,20 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using Domain.Abstract;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
