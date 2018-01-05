4a849e6e5137f4b634c0ebb994b4a78a79c10a59
LadyHail <ladyhail@outlook.com>
2018-01-05 20:40:10 +0100

    Adding/removing items to cart is possible.

---
+++ WebUI/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+    }
+}
